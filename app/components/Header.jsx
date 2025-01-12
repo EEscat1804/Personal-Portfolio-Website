4228ef20989c700045d8c9ad046a692a55e714fb
Ethan Escat <142355412+EEscat1804@users.noreply.github.com>
2025-01-11 23:11:57 -0800

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = ({isDarkMode}) => {
     whileInView={{y: 0, opacity: 1}}
     transition={{duration: 0.8, delay: 0.5}}
     className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
-        Aspring Engineer based in California.</motion.h1>
+        Aspiring Engineer based in California.</motion.h1>
 
         <motion.p 
         initial={{opacity: 0}}
